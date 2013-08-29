e66bfa502a060cf438d12e1708dc2b15bd8ebd18
Peter Sharp <peter@petersharp.org>
2013-08-29 16:06:09 +0100

    Beautified

--- js/seatingPlan.js
+++ js/seatingPlan.js
@@ -1,9 +1,8 @@
 // shuffle from http://stackoverflow.com/questions/2450954/how-to-randomize-a-javascript-array
+
 function shuffle(array) {
-  var currentIndex = array.length
-    , temporaryValue
-    , randomIndex
-    ;
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
@@ -22,52 +21,50 @@ function shuffle(array) {
 }
 
 // Load jquery
-(function(){
+(function() {
   var newscript = document.createElement('script');
-     newscript.type = 'text/javascript';
-     newscript.async = true;
-     newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
-  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
+  newscript.type = 'text/javascript';
+  newscript.async = true;
+  newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(newscript);
   var newscript = document.createElement('script');
-     newscript.type = 'text/javascript';
-     newscript.async = true;
-     newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
-  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
+  newscript.type = 'text/javascript';
+  newscript.async = true;
+  newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(newscript);
 })();
 
+// call main when jQuery is loaded
 function checkJquery() {
-    if (window.jQuery) {
-        main();
-    } else {
-        window.setTimeout(checkJquery, 100);
-    }
+  if (window.jQuery) {
+    main();
+  } else {
+    window.setTimeout(checkJquery, 100);
+  }
 }
 
 function main() {
-	// Load style sheet
+  // Load style sheet
   var url = 'http://mrpsharp.org/css/seatingplan.css';
-  if (document.createStyleSheet)
-  {
-      document.createStyleSheet(url);
-  }
-  else
-  {
-      $('<link rel="stylesheet" type="text/css" href="' + url + '" />').appendTo('head'); 
+  if (document.createStyleSheet) {
+    document.createStyleSheet(url);
+  } else {
+    $('<link rel="stylesheet" type="text/css" href="' + url + '" />').appendTo('head');
   }
 
   // Mirror function
 
   $cells = $("td:contains('Name')").parent().siblings().children("td:first-child"); // get name cells plus bottom
-	var names = [];
-	$cells.each(function() {
-		if ($(this).text().charAt(0) == '(') {
-			return false;
-		} else {
-			names.push($(this).text());
-		}
-	});
-	shuffle(names);
-	var overlay = jQuery('<div id="overlay"><h1>Seating Plan</h1> \
+  var names = [];
+  $cells.each(function() {
+    if ($(this).text().charAt(0) == '(') {
+      return false;
+    } else {
+      names.push($(this).text());
+    }
+  });
+  shuffle(names);
+  var overlay = jQuery('<div id="overlay"><h1>Seating Plan</h1> \
     <p style="float:right;"><a style=" color=red; " href="#" id="clear"><< Clear >></a></p> \
     <form id="layout_form"> \
     <label for="layout">Enter number of seats in each row, separated with spaces</label> \
@@ -78,17 +75,23 @@ function main() {
     <table id="seat_table"></table></div>');
   overlay.appendTo(document.body);
   $('#mirror').hide();
-  $('#clear').click(function() { $('#overlay').hide(); return false;});
-  setTimeout(function(){
-            $("#layout").focus();
-        }, 1);
+  $('#clear').click(function() {
+    $('#overlay').hide();
+    return false;
+  });
+  setTimeout(function() {
+    $("#layout").focus();
+  }, 1);
   var mirror = document.getElementById('mirror');
   mirror.onclick = function() {
-    for (i=0; i<rows.length; i++) {
-      rows[0].parentNode.insertBefore(rows[rows.length-1], rows[i]);};
-    for (row =0; row<rows.length;row++){
-      cells=rows[row].getElementsByTagName('td');
-      for (i=0; i<cells.length; i++) {cells[0].parentNode.insertBefore(cells[cells.length-1], cells[i])};
+    for (i = 0; i < rows.length; i++) {
+      rows[0].parentNode.insertBefore(rows[rows.length - 1], rows[i]);
+    };
+    for (row = 0; row < rows.length; row++) {
+      cells = rows[row].getElementsByTagName('td');
+      for (i = 0; i < cells.length; i++) {
+        cells[0].parentNode.insertBefore(cells[cells.length - 1], cells[i])
+      };
     };
     return false;
   }
@@ -96,14 +99,16 @@ function main() {
   rows = tbl.getElementsByTagName('tr');
   cells = []
   maxLength = 0
-  for (i=0; i<rows.length;i++){ 
+  for (i = 0; i < rows.length; i++) {
     rowLength = rows[i].childNodes.length
-    if (rowLength>maxLength) { maxLength = rowLength ;}
+    if (rowLength > maxLength) {
+      maxLength = rowLength;
     }
-  for (i=0; i<rows.length;i++){ 
+  }
+  for (i = 0; i < rows.length; i++) {
     difference = maxLength - rows[i].childNodes.length
     if (difference) {
-      for (j=0; j<difference; j++) {
+      for (j = 0; j < difference; j++) {
         cell = rows[i].insertCell(-1);
         cell.className = "grayed";
       }
@@ -116,7 +121,7 @@ function main() {
     rows = layout_str.split(" ");
     $.each(rows, function() {
       rowStr = "<tr>"
-      for (var i=0; i<this; i++) {
+      for (var i = 0; i < this; i++) {
         if (names.length > 0) {
           rowStr += "<td>" + names.pop() + "</td>"
         }
@@ -124,46 +129,46 @@ function main() {
       rowStr += "</tr>";
       $('#seat_table').append(rowStr);
     });
-    if (names.length >0) {
+    if (names.length > 0) {
       alert("Not enough seats!")
     }
     $('#layout_form').hide();
     $('#mirror').show();
     var mirror = document.getElementById('mirror');
-  mirror.onclick = function() {
-    for (i=0; i<rows.length; i++) {
-      rows[0].parentNode.insertBefore(rows[rows.length-1], rows[i]);
-    };
-    for (row =0; row<rows.length;row++){
-      cells=rows[row].getElementsByTagName('td');
-      for (i=0; i<cells.length; i++) {cells[0].parentNode.insertBefore(cells[cells.length-1], cells[i])};
-    };
-    return false;
-  }
-  tbl = document.getElementById('seat_table');
-  rows = tbl.getElementsByTagName('tr');
-  cells = []
-  maxLength = 0
-  for (i=0; i<rows.length;i++){ 
-    rowLength = rows[i].childNodes.length
-    if (rowLength>maxLength) { maxLength = rowLength ;}
+
+    mirror.onclick = function() {
+      for (i = 0; i < rows.length; i++) {
+        rows[0].parentNode.insertBefore(rows[rows.length - 1], rows[i]);
+      };
+      for (row = 0; row < rows.length; row++) {
+        cells = rows[row].getElementsByTagName('td');
+        for (i = 0; i < cells.length; i++) {
+          cells[0].parentNode.insertBefore(cells[cells.length - 1], cells[i])
+        };
+      };
+      return false;
     }
-  for (i=0; i<rows.length;i++){ 
-    difference = maxLength - rows[i].childNodes.length
-    if (difference) {
-      for (j=0; j<difference; j++) {
-        cell = rows[i].insertCell(-1);
-        cell.className = "grayed";
+    tbl = document.getElementById('seat_table');
+    rows = tbl.getElementsByTagName('tr');
+    cells = [];
+    maxLength = 0;
+    for (i = 0; i < rows.length; i++) {
+      rowLength = rows[i].childNodes.length
+      if (rowLength > maxLength) {
+        maxLength = rowLength;
       }
     }
-  }
+    for (i = 0; i < rows.length; i++) {
+      difference = maxLength - rows[i].childNodes.lengthj
+      if (difference) {
+        for (j = 0; j < difference; j++) {
+          cell = rows[i].insertCell(-1);
+          cell.className = "grayed";
+        }
+      }
+    }
     return false;
   });
 }
 
 checkJquery();
-
-
-
-
-
