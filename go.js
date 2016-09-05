34189a56c42b5c117266cdb14d2c5eda3ce2c097
Peter Sharp <peter@petersharp.org>
2016-09-05 17:16:06 +0100

    added shortcode generator

--- go.js
+++ go.js
@@ -1,6 +1,6 @@
 if (location.search) {
   var goCodes = {
-      'electricity-ii-hw-1': 'https://westminsterschool-my.sharepoint.com/personal/peter_sharp_westminster_org_uk/_layouts/15/guestaccess.aspx?guestaccesstoken=l0YGndfu4zxX1f5fuyuwWq5U7FWQ%2bWcPBfn36o9UpE0%3d&docid=0a62f8e054af8434385a3a1c6d95a6695&rev=1',
+      'el2hw1': 'https://westminsterschool-my.sharepoint.com/personal/peter_sharp_westminster_org_uk/_layouts/15/guestaccess.aspx?guestaccesstoken=l0YGndfu4zxX1f5fuyuwWq5U7FWQ%2bWcPBfn36o9UpE0%3d&docid=0a62f8e054af8434385a3a1c6d95a6695&rev=1',
   };
   var queryDict = {};
   location.search.substr(1).split("&").forEach(function(item) {
