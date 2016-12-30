8e73ce0d34ea09cda30f2fdc7ce81d24a3714e59
Peter Sharp <peter@petersharp.org>
2016-12-30 19:10:55 +0000

    added logic for rewrite

--- go.js
+++ go.js
@@ -5,15 +5,8 @@ var goCodes = {
   'tyc-winter-2016': 'https://docs.google.com/spreadsheets/d/1Bn4HjvzrLdk9y8QpalSb8m6HFhSqSv49HmvaXcmomig/edit?usp=sharing',
   'd3sow': ' https://westminsterschool-my.sharepoint.com/personal/peter_sharp_westminster_org_uk/_layouts/15/guestaccess.aspx?guestaccesstoken=h6Y9m9o8871vaN1MyFHvalOkALSng5INLJbDfMy%2f2yk%3d&docid=06dbfc14e13dc4504a97b503bf459a293&rev=1'
 };
-var $shortcodeStatus = $('#shortcode-status')
-if (location.search) {
-  //console.log("parsing GET....")
-  $shortcodeStatus.text('Looking up shortcode');
-  var queryDict = {};
-  location.search.substr(1).split("&").forEach(function(item) {
-      queryDict[item.split("=")[0]] = item.split("=")[1]
-  });
-  var shortCode = queryDict['l'];
+
+function go(shortCode) {
   if (goCodes[shortCode]) {
       $shortcodeStatus.html('Going to address: <a href="' + goCodes[shortCode] + '">'+goCodes[shortCode]+'</a>');
       document.location.href = goCodes[shortCode]
@@ -22,15 +15,22 @@ if (location.search) {
     window.setTimeout(function() {
      window.location.href = "https://www.mrpsharp.org";
     }, 5000);
-  } else {
+  }
+}
+var $shortcodeStatus = $('#shortcode-status')
+if (location.search) {
+  //console.log("parsing GET....")
+  $shortcodeStatus.text('Looking up shortcode');
+  var queryDict = {};
+  location.search.substr(1).split("&").forEach(function(item) {
+      queryDict[item.split("=")[0]] = item.split("=")[1]
+  });
+  go(queryDict['l']);
+} else if (window.location.host.split(":")[0]=="go.mrpsharp.org") {
+    go(window.location.pathname.substr(1));
+} else {
     $shortcodeStatus.text('No shortcode specified, redirecting to home...');
     window.setTimeout(function() {
       window.location.href = "https://www.mrpsharp.org";
     }, 5000);
-  }
-} else {
-  $shortcodeStatus.text('No shortcode specified, redirecting to home...');
-  window.setTimeout(function() {
-    window.location.href = "https://www.mrpsharp.org";
-  }, 5000);
 }
