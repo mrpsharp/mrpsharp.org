fa11468115e76acbab7fe50c8a321e055b253ba8
Peter Sharp <peter@petersharp.org>
2017-01-17 13:22:13 +0000

    Added bohr atom

---
+++ bohr.js
@@ -0,0 +1,53 @@
+function kineticEnergy(r, n) {
+  return n*n*h*h/(8*PI*PI*me*r*r*e);
+}
+
+function potentialEnergy(r) {
+  return -(ke*e/r);
+}
+
+function drawGraph() {
+  var n =document.getElementById('nValue').value;
+  var r = [], KE = [], PE = [], E = [] // array of radii
+  for (var i=0.1;i<=10;i+=0.01) {
+    r.push(i); // r in angstrom
+    var tempKE = kineticEnergy(i*1e-10, n);
+    var tempPE = potentialEnergy(i*1e-10)
+    KE.push(tempKE);
+    PE.push(tempPE);
+    E.push(tempPE+tempKE);
+  }
+  var keTrace = {
+    x: r,
+    y: KE,
+    type: 'scatter',
+    name: 'Kinetic Energy'
+  };
+  var peTrace = {
+    x: r,
+    y: PE,
+    type: 'scatter',
+    name: 'Potential Energy'
+  };
+  var eTrace = {
+    x: r,
+    y: E,
+    type: 'scatter',
+    name: 'Total Energy'
+  };
+
+  var data = [keTrace, peTrace, eTrace];
+  var layout = {
+    xaxis: {
+      title: "R / angstroms"
+    },
+    yaxis: {
+      title: "Energy / eV",
+      range: [-15,5]
+    }
+  }
+  Plotly.newPlot('graphDiv', data, layout);
+  console.log('Graph drawn');
+}
+
+document.addEventListener('DOMContentLoaded', drawGraph(1), false);
---
+++ const.js
@@ -0,0 +1,6 @@
+var PI = Math.PI,
+    h = 6.63e-34, // Plank's constant
+    me = 9.11e-31, // Electron mass
+    e = 1.6e-19, // Fundamental charge
+    ke = 9e9 // Electrostatic constants
+    ;
