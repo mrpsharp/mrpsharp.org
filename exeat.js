de656df3aba2bf681f04480b6de7f931ad2b3b77
Peter Sharp <peter@petersharp.org>
2019-03-27 15:39:32 +0000

    Update exeat.js

--- exeat.js
+++ exeat.js
@@ -1,11 +1,5 @@
 document.getElementById('top').style.cssText = "display: None";
-document.getElementById('wrapper').style.cssText = "position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    -webkit-transform: translate(-50%, -50%);
-    -o-transform: translate(-50%, -50%);
-    -ms-transform: translate(-50%, -50%)";
+document.getElementById('wrapper').style.cssText = "position: absolute; left: 50%; top: 50%;transform: translate(-50%, -50%);-webkit-transform: translate(-50%, -50%);-o-transform: translate(-50%, -50%);-ms-transform: translate(-50%, -50%)";
 var timerId =
   countdown(
     new Date(2019,02,29,12,0,0,0),
