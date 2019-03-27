8b61cdaf852d5c3185e8709020fc6fdfcd8631cd
Peter Sharp <peter@petersharp.org>
2019-03-27 14:00:51 +0000

    Update exeat.js

--- exeat.js
+++ exeat.js
@@ -1,6 +1,6 @@
 var timerId =
   countdown(
-    new Date(2019,03,29,12,0,0,0),
+    new Date(2019,02,29,12,0,0,0),
     function(ts) {
       document.getElementById('exeat').innerHTML = ts.toString();
     },
