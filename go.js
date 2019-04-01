1ec4746542857db7ffac7e2b8aa20f33ff5e1b0e
Peter Sharp <peter@petersharp.org>
2019-04-01 09:49:55 +0100

    Update go.js

--- go.js
+++ go.js
@@ -1,10 +1,5 @@
 var goCodes = {
-  'tyc-2017': 'https://docs.google.com/spreadsheets/d/1oHwlnHY2S_bsgnSHToZTq3q29j6eNZqvF8e4vRzIRQQ/edit?usp=sharing',
-  'el2hw1': 'https://westminsterschool-my.sharepoint.com/personal/peter_sharp_westminster_org_uk/_layouts/15/guestaccess.aspx?guestaccesstoken=l0YGndfu4zxX1f5fuyuwWq5U7FWQ%2bWcPBfn36o9UpE0%3d&docid=0a62f8e054af8434385a3a1c6d95a6695&rev=1',
-  'westminster': 'https://www.westminster.org.uk',
-  'a1hw1': 'https://westminsterschool-my.sharepoint.com/personal/peter_sharp_westminster_org_uk/_layouts/15/guestaccess.aspx?guestaccesstoken=iyAhQno9k728GCWvGdE5BpT46NozKP0aMfJ%2bSvXTZ%2fI%3d&docid=0d436f931bdca4d87bfec8c5b26785f83&rev=1',
-  'tyc-winter-2016': 'https://docs.google.com/spreadsheets/d/1Bn4HjvzrLdk9y8QpalSb8m6HFhSqSv49HmvaXcmomig/edit?usp=sharing',
-  'd3sow': ' https://westminsterschool-my.sharepoint.com/personal/peter_sharp_westminster_org_uk/_layouts/15/guestaccess.aspx?guestaccesstoken=h6Y9m9o8871vaN1MyFHvalOkALSng5INLJbDfMy%2f2yk%3d&docid=06dbfc14e13dc4504a97b503bf459a293&rev=1'
+  'WHW' : 'https://drive.google.com/open?id=1vJXtP076R_a2DFcuIKCIiqaZwp5P6K5d&usp=sharing'
 };
 
 function go(shortCode) {
